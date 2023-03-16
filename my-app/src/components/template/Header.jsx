5fd0ab9399f083a085d964b944c96b1209e839ac
Liviu Deleu <liviu.deleu.2002@gmail.com>
2023-03-16 23:23:28 +0200

    menu

---
+++ my-app/src/components/template/Header.jsx
@@ -0,0 +1,29 @@
+import { Container } from "react-bootstrap";
+import './menu.css'
+import Basket from "../../../src/assets/images/Menu/basket.png"
+import Favorites from "../../../src/assets/images/Menu/heart.png"
+import User from "../../../src/assets/images/Menu/user.png"
+import Search from "../../../src/assets/images/Menu/search.png"
+
+
+export default function Header(){
+    return (
+        <>
+            <div id="Menu">
+                <button>Call Center</button>
+                <nav>
+                    <a href="#" id="TextYellow">Home</a>
+                    <a href="#">Shop</a>
+                    <a href="#">Contacts</a>
+                </nav>
+                <img src={Search} id="search"></img>
+                <input type="text" placeholder="Search"></input>
+                <container>
+                   <img src={Basket}/>
+                   <img src={Favorites}/>
+                   <img src={User}/>
+                </container>
+            </div>
+        </>
+    )
+}
