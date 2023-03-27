1135b8a8975486a86f9f98e6b886631638ca7cd7
Liviu Deleu <liviu.deleu.2002@gmail.com>
2023-03-27 13:46:10 +0300

    StrapiElement

--- my-app/src/Pages/PageShop/ShopPage.jsx
+++ my-app/src/Pages/PageShop/ShopPage.jsx
@@ -1,8 +1,36 @@
+import axios from "axios";
+import "../PageShop/ShopPage.css"
+import { useEffect, useState } from "react";
 
-export default function ShopPage(){
-    return(
-        <content>
-            ssssssssssssss
-        </content>
-    )
-}
+const Products = () => {
+    const [error, setError] = useState(null);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios
+          .get("http://localhost:1337/api/products?populate=ProductMedia")
+          .then(({ data }) => setProducts(data.data))
+          .catch((error) => setError(error));
+      }, []);
+
+      if (error) {
+        // Print errors if any
+        return <div>An error occured: {error.message}</div>;
+      }
+
+      return (<>
+        <div id="ProductTopSpace">
+        {products.map(({ id, attributes }) => (
+            <a key={id}>
+              <div id="ProductCard">
+              <img id="ShopProductImage" src={`http://localhost:1337${attributes.ProductMedia.data[0].attributes.url}`} alt=""/>
+                <p id="ProductTextName">{attributes.ProductText}</p>
+                <p id="ProductRichTextName">{attributes.ProductRichText}</p>
+              </div>
+            </a>
+          ))}
+        </div>
+    </>);
+  };
+  
+  export default Products;
